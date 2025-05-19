18a4140e2ce1a1a21535051b15299ec51f15bd75
qilihui <1499910521@qq.com>
2025-05-19 16:44:24 +0800

    init

---
+++ upload_server.py
@@ -0,0 +1,47 @@
+
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def index():
+    files = []
+    for filename in os.listdir(UPLOAD_FOLDER):
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+        ctime = datetime.fromtimestamp(os.path.getctime(filepath))
+        files.append({
+            'name': filename,
+            'upload_time': ctime.strftime('%Y-%m-%d %H:%M:%S'),
+            'timestamp': ctime.timestamp()
+        })
+    # 按上传时间降序排序
+    files.sort(key=lambda x: x['timestamp'], reverse=True)
+    return render_template('index.html', files=files)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+    return redirect(url_for('index'))
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+
+@app.route('/delete/<filename>')
+def delete_file(filename):
+    filepath = os.path.join(UPLOAD_FOLDER, filename)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
